e487a6941c933a9bf7da444cbf6849cfde1536ce
wxingyue-umich <wxingyue@umich.edu>
2026-04-23 19:01:32 -0400

    Add interactivity in script.js

--- scripts/start.js
+++ scripts/start.js
@@ -1,13 +1,3 @@
-const { execSync } = require('child_process')
-
-const port = process.env.PORT || 8080
-let cmd = `serve --listen ${port}  --no-clipboard`
-
-if (process.argv.length > 2) cmd += ` ${process.argv.slice(2).join(' ')}`
-
-// eslint-disable-next-line no-console
-console.log(`Running "${cmd}"...`)
-console.log('****IF THE SERVER(s) LISTED HAVE PORT :10000, do not try to visit/open it, it is only for the test.***')
-
-
-execSync(cmd, { stdio: 'inherit' })
+document.querySelector('#button').addEventListener('click', () => {
+    document.querySelector('#output').textContent = "World";
+})
